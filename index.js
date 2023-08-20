89e146a1973b5517b4f7ec5cc24393e4d26a4449
Joe <joedsonfera@hotmail.com>
2023-08-20 11:37:34 -0300

    player movement update

--- index.js
+++ index.js
@@ -29,29 +29,50 @@ class Player {
   draw() {
     // c.fillStyle = 'red'
     // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    if (this.image)
-      c.drawImage(
-        this.image,
-        this.position.x,
-        this.position.y,
-        this.width,
-        this.height
-      )
+
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    )
   }
 
   update() {
-    this.draw()
-    this.position.x += this.velocity.x
+    if (this.image) {
+      this.draw()
+      this.position.x += this.velocity.x
+    }
   }
 }
 
 const player = new Player()
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  space: {
+    pressed: false
+  }
+}
 
 function animate() {
   requestAnimationFrame(animate)
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
+
+  if (keys.a.pressed) {
+    player.velocity.x = -5
+  } else if (keys.d.pressed) {
+    player.velocity.x = 5
+  } else {
+    player.velocity.x = 0
+  }
 }
 
 animate()
@@ -60,9 +81,27 @@ addEventListener('keydown', ({ key }) => {
   switch (key) {
     case 'a':
       console.log('left')
+      keys.a.pressed = true
       break
     case 'd':
       console.log('right')
+      keys.d.pressed = true
+      break
+    case ' ':
+      console.log('space')
+      break
+  }
+})
+
+addEventListener('keyup', ({ key }) => {
+  switch (key) {
+    case 'a':
+      console.log('left')
+      keys.a.pressed = false
+      break
+    case 'd':
+      console.log('right')
+      keys.d.pressed = false
       break
     case ' ':
       console.log('space')
