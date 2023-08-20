c39626388ac51110288ebdd33a1224a531ea1865
Joe <joedsonfera@hotmail.com>
2023-08-20 14:20:46 -0300

    projectile update

--- index.js
+++ index.js
@@ -63,7 +63,33 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position
+    this.velocity = velocity
+
+    this.radius = 3
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    c.fillStyle = 'red'
+    c.fill()
+    c.closePath()
+  }
+
+  update() {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+  }
+}
+
 const player = new Player()
+const projectiles = [
+
+]
 const keys = {
   a: {
     pressed: false
@@ -81,6 +107,9 @@ function animate() {
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
   player.update()
+  projectiles.forEach(projectile => {
+    projectile.update()
+  })
 
   if (keys.a.pressed && player.position.x >= 0) {
     player.velocity.x = -6
@@ -108,6 +137,16 @@ addEventListener('keydown', ({ key }) => {
       break
     case ' ':
       console.log('space')
+      projectiles.push(new Projectile({
+        position: {
+          x: 300,
+          y: 300
+        },
+        velocity: {
+          x: 0,
+          y: -5
+        }
+      }))
       break
   }
 })
